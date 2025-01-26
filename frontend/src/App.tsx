3f7726c6ae2eb42d53b0dc1ed898e5a421bfae5e
shreyashchandra <shreyash.chandra123@gmail.com>
2025-01-27 01:03:27 +0530

    Add routing and initial page components for Signup, Signin, Blog, and Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup, Signin, Blog, Dashboard } from "./pages";
 
 function App() {
   return (
     <>
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,6 @@
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Blog from "./Blog";
+import Dashboard from "./Dashboard";
+
+export { Signup, Signin, Blog, Dashboard };
